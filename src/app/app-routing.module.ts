0358a720df8214a0816df12b00debdc69e12570a
Gabriel Costa <gacosil0204@gmail.com>
2022-01-20 16:14:52 -0300

    Rotas funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: 'entrar', pathMatch: 'full'
-  },
-
-  {
-    path: 'entrar', component: EntrarComponent
-  },
-  {
-    path: 'cadastro', component: CadastroComponent
-  }
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: "sobre-nos", component: SobreNosComponent}
 ];
 
 @NgModule({
