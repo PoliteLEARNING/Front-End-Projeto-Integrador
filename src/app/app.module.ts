90d358bf3685f5fe973ee3cc906142e72688c13f
gersonespejo127@gmail.com <gersonespejo127@gmail.com>
2022-01-20 13:36:41 -0300

    Implementação dos componentes entrar e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
