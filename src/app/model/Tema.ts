6470b0af3e87c4a772764a49c77afe5ca8c59c4b
Gabriel Costa <gacosil0204@gmail.com>
2022-02-03 22:38:55 -0300

    .

--- src/app/model/Tema.ts
+++
@@ -1,9 +0,0 @@
-import { Postagem } from "./Postagem"
-
-export class Tema{
-    public id: number
-    public descricao: string
-    public imagem: string
-    public relacionados: string
-    public postagem: Postagem[]
-}
